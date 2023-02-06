e6e1ef1fe807d9b4b51c44daad870e9d11db235b
Killian Hourihan <killianhourihan@gmail.com>
2023-02-06 10:28:45 -0800

    working astar

--- src/field.rs
+++ src/field.rs
@@ -1,139 +1,7 @@
 use macroquad::prelude::*;
 #[path = "snake.rs"] mod snake;
-use self::snake::{Snake};
+use self::snake::{Snake, Cell};
 
-
-
-pub struct Cell {
-    x: usize,
-    y: usize,
-    contents: u8,
-    snake_segment: isize
-}
-
-impl Cell {
-    pub fn new(x: usize, y: usize) -> Self {
-        Self {
-            x: x,
-            y: y,
-            contents: 0,
-            snake_segment: -1
-        }
-    }
-    pub fn is_empty(&self) -> bool {
-        self.contents == 0
-    }
-    pub fn is_food(&self) -> bool {
-        self.contents == 1
-    }
-    pub fn is_segment(&self) -> bool {
-        self.contents == 2
-    }
-    pub fn is_head(&self) -> bool {
-        self.contents == 3
-    }
-    pub fn is_snake(&self) -> bool {
-        (self.contents == 2) || (self.contents == 3)
-    }
-    pub fn set_empty(&mut self) {
-        self.contents = 0;
-    }
-    pub fn set_food(&mut self) {
-        self.contents = 1;
-    }
-    pub fn set_segment(&mut self, index: usize) {
-        self.contents = 2;
-        self.snake_segment = index as isize;
-    }
-    pub fn set_head(&mut self) {
-        self.contents = 3;
-    }
-    pub fn pos(&self) -> (isize, isize) {
-        (self.x as isize, self.y as isize)
-    }
-    pub fn draw(&self, tilesize: f32, neighbors: Vec<bool>) {
-        let x_pos = self.x as f32 * tilesize;
-        let y_pos = self.y as f32 * tilesize;
-        let padding = tilesize / 20.0;
-
-        match self.contents {
-            1 => {
-                draw_circle(x_pos + tilesize / 2.0, y_pos + tilesize / 2.0, (tilesize - padding * 2.0) / 2.0, Color::from_rgba(224, 176, 31, 255));
-            },
-            2 => {
-                draw_circle(x_pos + tilesize / 2.0, y_pos + tilesize / 2.0, (tilesize - padding * 2.0) / 2.0, Color::from_rgba(45, 148, 10, 255));
-                for (dir, value) in neighbors.iter().enumerate() {
-                    if *value {
-                        let mut pos = (x_pos, y_pos);
-                        let mut dims = (tilesize, tilesize);
-                        match dir {
-                            0 => {
-                                pos.0 += padding;
-                                dims.0 -= padding * 2.0;
-                                dims.1 /= 2.0;
-                            },
-                            1 => {
-                                pos.0 += padding;
-                                pos.1 += tilesize / 2.0;
-                                dims.0 -= padding * 2.0;
-                                dims.1 /= 2.0;
-                            },
-                            2 => {
-                                pos.1 += padding;
-                                dims.1 -= padding * 2.0;
-                                dims.0 /= 2.0;
-                            },
-                            3 => {
-                                pos.1 += padding;
-                                pos.0 += tilesize / 2.0;
-                                dims.1 -= padding * 2.0;
-                                dims.0 /= 2.0;
-                            },
-                            _ => {}
-                        }
-                        draw_rectangle(pos.0, pos.1, dims.0, dims.1, Color::from_rgba(45, 148, 10, 255));
-                    }
-                }
-            },
-            3 => {
-                draw_circle(x_pos + tilesize / 2.0, y_pos + tilesize / 2.0, (tilesize - padding * 2.0) / 2.0, Color::from_rgba(150, 235, 89, 255));
-                for (dir, value) in neighbors.iter().enumerate() {
-                    if *value {
-                        let mut pos = (x_pos, y_pos);
-                        let mut dims = (tilesize, tilesize);
-                        match dir {
-                            0 => {
-                                pos.0 += padding;
-                                dims.0 -= padding * 2.0;
-                                dims.1 /= 2.0;
-                            },
-                            1 => {
-                                pos.0 += padding;
-                                pos.1 += tilesize / 2.0;
-                                dims.0 -= padding * 2.0;
-                                dims.1 /= 2.0;
-                            },
-                            2 => {
-                                pos.1 += padding;
-                                dims.1 -= padding * 2.0;
-                                dims.0 /= 2.0;
-                            },
-                            3 => {
-                                pos.1 += padding;
-                                pos.0 += tilesize / 2.0;
-                                dims.1 -= padding * 2.0;
-                                dims.0 /= 2.0;
-                            },
-                            _ => {}
-                        }
-                        draw_rectangle(pos.0, pos.1, dims.0, dims.1, Color::from_rgba(150, 235, 89, 255));
-                    }
-                }
-            },
-            _ => {}
-        }
-    }
-}
 
 
 pub struct Field {
@@ -164,7 +32,7 @@ impl Field {
     }
     pub fn update(&mut self) {
         self.snake.input();
-        self.snake.update(&mut self.food);
+        self.snake.update(&mut self.food, &self.cells);
     }
     fn get_neighbors(&self, cell: &Cell) -> Vec<bool> {
         let mut neighbors: Vec<bool> = Vec::new();
--- src/snake.rs
+++ src/snake.rs
@@ -1,7 +1,12 @@
 use std::time::{SystemTime, Duration};
+use std::collections::{HashMap, HashSet};
+use priority_queue::PriorityQueue;
 
-use macroquad::{prelude::{is_key_pressed, KeyCode}};
+use macroquad::prelude::{is_key_pressed, KeyCode};
 use rand::distributions::{Uniform, Distribution};
+
+#[path = "cell.rs"] mod cell;
+pub use cell::Cell;
 
 
 pub enum Directions {
@@ -21,6 +26,7 @@ pub struct Snake {
     pub score: usize,
     grid_size: usize,
     dir: (i8, i8),
+    optimal_path: Vec<(isize, isize)>,
 
     time: SystemTime,
     time_step: Duration,
@@ -42,6 +48,7 @@ impl Snake {
             segments: Vec::from([(xpos, ypos)]),
             length: 1,
             dir: (0, 0),
+            optimal_path: Vec::new(),
 
             time: SystemTime::now(),
             time_step: Duration::from_millis(110),
@@ -119,12 +126,121 @@ impl Snake {
         food.0 = x;
         food.1 = y;
     }
-    pub fn update(&mut self, food: &mut (isize, isize)) {
+
+
+    fn get_neighbor_cells(&self, grid: &Vec<Vec<Cell>>, cell: &Cell) -> Vec<(isize, isize)> {
+        let mut neighbors = Vec::new();
+        for row in grid.iter() {
+            for other in row.iter() {
+                if (other.x as isize - cell.x as isize).abs() + (other.y as isize - cell.y as isize).abs() != 1 {
+                    continue;
+                }
+                if !other.is_snake() {
+                    neighbors.push((other.x as isize, other.y as isize));
+                }
+            };
+        };
+        return neighbors;
+    }
+    fn create_graph(&self, grid: &Vec<Vec<Cell>>) -> HashMap<(isize, isize), Vec<(isize, isize)>> {
+        let mut graph = HashMap::<(isize, isize), Vec<(isize, isize)>>::new();
+        for row in grid.iter() {
+            for cell in row.iter() {
+                let neighbors = self.get_neighbor_cells(grid, cell);
+                if neighbors.len() > 0 {
+                    graph.insert((cell.x as isize, cell.y as isize), neighbors);
+                }
+            };
+        };
+        return graph;
+    }
+    fn heuristic(&self, p1: (isize, isize), p2: (isize, isize)) -> isize {
+        return (p1.0 - p2.0).abs() + (p1.1 - p2.1).abs();
+    }
+    fn astar(&self, food: &mut (isize, isize), grid: &Vec<Vec<Cell>>, graph: HashMap<(isize, isize), Vec<(isize, isize)>>) -> HashMap<(isize, isize), (isize, isize)> {
+        let mut count = 0;
+        let mut open_set = PriorityQueue::new();
+        open_set.push((count, (self.x, self.y)), 0);
+
+        let mut visited = HashMap::new();
+        let mut g_score = HashMap::new();
+        let mut f_score = HashMap::new();
+        let mut open_set_hash = HashSet::new();
+        open_set_hash.insert((self.x, self.y));
+        visited.insert((self.x, self.y), (-1, -1));
+
+        for row in grid.iter() {
+            for cell in row.iter() {
+                g_score.insert((cell.x as isize, cell.y as isize), isize::MAX);
+                f_score.insert((cell.x as isize, cell.y as isize), isize::MAX);
+            }
+        }
+        g_score.insert((self.x, self.y), 0);
+        f_score.insert((self.x, self.y), self.heuristic((self.x, self.y), food.clone()));
+
+        while !open_set.is_empty() {
+            let cur_value = open_set.pop().unwrap().0;
+            let current = cur_value.1;
+            open_set_hash.remove(&current);
+
+            if current == *food {
+                break
+            }
+            let v: Vec<(isize, isize)> = Vec::new();
+            let neighbors = match graph.get(&current) {
+                Some(value) => value,
+                None => &v,
+            };
+
+            for neigh in neighbors.iter() {
+                let temp_g_score = g_score.get(&current).unwrap() + 1;
+
+                if temp_g_score < *g_score.get(neigh).unwrap() {
+                    visited.insert(*neigh, current);
+                    g_score.insert(*neigh, temp_g_score);
+                    f_score.insert(*neigh, temp_g_score + self.heuristic(*neigh, *food));
+                    if !open_set_hash.contains(neigh) {
+                        count += 1;
+                        open_set.push((count, *neigh), -*f_score.get(neigh).unwrap());
+                        open_set_hash.insert(*neigh);
+                    };
+                };
+            };
+        };
+        return visited;
+    }
+
+    fn reconstruct_path(&self, route: &mut Vec<(isize, isize)>, visited: HashMap<(isize, isize), (isize, isize)>, food: &mut (isize, isize)) {
+        let mut path_segment = *food;
+        while (path_segment != (-1, -1)) && visited.contains_key(&path_segment) {
+            route.push(path_segment);
+            path_segment = *visited.get(&path_segment).unwrap();
+        }
+    }
+    fn create_path(&self, food: &mut (isize, isize), grid: &Vec<Vec<Cell>>) -> Vec<(isize, isize)> {
+        let mut route = Vec::new();
+        let graph = self.create_graph(grid);
+        let visited = self.astar(food, grid, graph);
+        if visited.len() > 0 {
+            self.reconstruct_path(&mut route, visited, food);
+        }
+        return route;
+    }
+    pub fn update_compute(&mut self, food: &mut (isize, isize), grid: &Vec<Vec<Cell>>) {
+        self.optimal_path = self.create_path(food, grid);
+        if self.optimal_path.len() > 0 {
+            self.optimal_path.pop().unwrap();
+            (self.x, self.y) = self.optimal_path.pop().unwrap();
+        }
+    }
+
+    pub fn update(&mut self, food: &mut (isize, isize), grid: &Vec<Vec<Cell>>) {
         let time_now = SystemTime::now();
 
         if time_now.duration_since(self.time).unwrap() > self.time_step {
             self.time = time_now;
 
+            self.update_compute(food, grid);
             self.x += self.dir.0 as isize;
             self.y += self.dir.1 as isize;
             self.segments.push((self.x, self.y));
@@ -134,7 +250,6 @@ impl Snake {
                 self.score += 1;
             }
             self.segments = self.segments[((self.segments.len() - self.length as usize) as usize)..].to_vec();
-
         }
 
         self.hit_body = self.segments[..(self.segments.len() - 1)].contains(&(self.x, self.y));
